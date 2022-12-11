45dd414cd48fa13953fb003239289fec15ebada7
reyfar <13643966+reyfar@users.noreply.github.com>
2022-12-11 11:16:08 +0100

    Reverting lr_decay.py

--- mingpt/lr_decay.py
+++ mingpt/lr_decay.py
@@ -14,9 +14,7 @@ class LearningRateDecayCallback(pl.Callback):
 
     def on_train_batch_end(self, trainer, pl_module, batch, batch_idx, dataloader_idx):
         optimizer = trainer.optimizers[0]
-        x, y = batch
-        print(x)
-        print(y)
+        _, y = batch
 
         if self.lr_decay:
             self.tokens += (y >= 0).sum()  # number of tokens processed this step (i.e. label is not -100)
